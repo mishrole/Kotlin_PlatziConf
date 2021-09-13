37b8de19f79b6bd7f59cfe62888897d4944346b4
Mitchell Rodríguez <mitrodle@gmail.com>
2021-09-13 17:56:17 -0500

    add: SplashscreenActivity base

---
+++ app/src/main/java/com/mishrole/platziconf/ui/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.mishrole.platziconf.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mishrole.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
