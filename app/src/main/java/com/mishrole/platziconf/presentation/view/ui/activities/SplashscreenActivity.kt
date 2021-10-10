d498f0fba16959d9e0a144deea26e970117ec8cf
Mitchell Rodríguez <mitrodle@gmail.com>
2021-10-10 03:34:09 -0500

    add: Splashscreen animation

--- app/src/main/java/com/mishrole/platziconf/presentation/view/ui/activities/SplashscreenActivity.kt
+++ app/src/main/java/com/mishrole/platziconf/presentation/view/ui/activities/SplashscreenActivity.kt
@@ -1,12 +1,41 @@
 package com.mishrole.platziconf.presentation.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.mishrole.platziconf.R
+import com.mishrole.platziconf.databinding.ActivitySplashscreenBinding
 
 class SplashscreenActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashscreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        binding.ivLogoPlatziConf.startAnimation(animation)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish() // Destroy this activity
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
     }
 }
