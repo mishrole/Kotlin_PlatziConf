984f8ce7fdd4056794455425e8db5b3fda80b8fe
Mitchell Rodríguez <mitrodle@gmail.com>
2021-10-10 03:19:03 -0500

    add: Google Maps + Style

--- app/src/main/java/com/mishrole/platziconf/presentation/view/ui/fragments/LocationDetailDialogFragment.kt
+++ app/src/main/java/com/mishrole/platziconf/presentation/view/ui/fragments/LocationDetailDialogFragment.kt
@@ -1,20 +1,71 @@
 package com.mishrole.platziconf.presentation.view.ui.fragments
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.mishrole.platziconf.R
+import com.mishrole.platziconf.core.entity.Location
+import com.mishrole.platziconf.databinding.FragmentLocationDetailDialogBinding
 
-class LocationDetailDialogFragment : Fragment() {
-   
+class LocationDetailDialogFragment : DialogFragment() {
+
+    private lateinit var binding : FragmentLocationDetailDialogBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location_detail_dialog, container, false)
+    ): View {
+        binding = FragmentLocationDetailDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbarLocation.navigationIcon = ContextCompat.getDrawable(binding.root.context, R.drawable.ic_icons_close)
+        binding.toolbarLocation.navigationIcon?.setTint(Color.WHITE)
+        binding.toolbarLocation.setTitleTextColor(Color.WHITE)
+        binding.toolbarLocation.setNavigationOnClickListener {
+            dismissAllowingStateLoss()
+        }
+
+        val location = Location()
+
+        binding.toolbarLocation.title = location.name
+        binding.tvDetailsLocationAddress.text = location.address
+        binding.tvDetailsLocationLink.text = location.website
+        binding.tvDetailsLocationPhone.text = location.phone
+        binding.tvDetailsLocationName.text = location.name
+
+        binding.tvDetailsLocationPhone.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${location.phone}")
+            }
+            startActivity(intent)
+        }
+
+        binding.tvDetailsLocationLink.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(location.website)
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 
 }
--- app/src/main/java/com/mishrole/platziconf/presentation/view/ui/fragments/LocationFragment.kt
+++ app/src/main/java/com/mishrole/platziconf/presentation/view/ui/fragments/LocationFragment.kt
@@ -1,20 +1,68 @@
 package com.mishrole.platziconf.presentation.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import com.mishrole.platziconf.core.entity.*
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
 import com.mishrole.platziconf.R
+import com.mishrole.platziconf.databinding.FragmentLocationBinding
 
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private lateinit var binding : FragmentLocationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+    ): View {
+        binding = FragmentLocationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_location) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val location = Location()
+        val zoom = 16f
+        val center = LatLng(location.latitude, location.longitude)
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center, zoom))
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(center)
+        markerOptions.title("Platzi Conf")
+
+        val bitmap = requireContext().applicationContext.let {
+            ContextCompat.getDrawable(it, R.drawable.logo_platzi)
+        } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmap.bitmap, 150, 150, false)
+
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+        googleMap.addMarker(markerOptions)
+        googleMap.setOnMarkerClickListener(this)
+        googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(), R.raw.custom_map))
+
+    }
+
+    override fun onMarkerClick(googleMap: Marker): Boolean {
+        findNavController().navigate(R.id.locationDetailFragmentDialog)
+        return true
     }
 
 }
